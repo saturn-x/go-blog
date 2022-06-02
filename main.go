a4e691481f5b5b85bf7c3dc90c53983c6bda19d3
Your Name <you@example.com>
2022-06-02 22:10:40 +0800

    commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/saturn-x/ginproject/pkg/setting"
+	"github.com/saturn-x/ginproject/routers"
+	"net/http"
+)
+
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
+func main() {
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
+}
---
+++ pkg/util/pagination.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/saturn-x/ginproject/pkg/setting"
+	"github.com/unknwon/com"
+)
+
+func GetPage(c *gin.Context) int {
+	result := 0
+	page, _ := com.StrTo(c.Query("page")).Int()
+	if page > 0 {
+		result = (page - 1) * setting.PageSize
+	}
+
+	return result
+}
